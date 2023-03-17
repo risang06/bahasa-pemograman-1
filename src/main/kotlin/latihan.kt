4aae01b270a9248b5c73561e6ab00a14335d4c38
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:15:58 +0700

    first commit

--- src/main/kotlin/latihan.kt
+++ src/main/kotlin/latihan.kt
@@ -1,14 +1,28 @@
-fun main(){
-    println("Hello World")
-    println("Risang")
-    //println("Brrr")
+//fun main(){
+//    println("Hello World")
+//    println("Risang")
+//    //println("Brrr")
+//
+//
+//    print("Pekerjaaan :")
+//    val Pekerjaan = readLine()
+//
+//    print("Alamat :")
+//    val Alamat = readLine()
+//
+//    println("Pekerjaaan kamu adalah $Pekerjaan dan alamat kamu di $Alamat")
+//}
 
+fun main(args: Array<String>){
+    var namaLengkap: String = "Risang"
 
-    print("Pekerjaaan :")
-    val Pekerjaan = readLine()
+    //Variabel yang kosong harus disebutkan tipe datanya
+    var alamat: String
+    alamat = "Yogyakarta"
 
-    print("Alamat :")
-    val Alamat = readLine()
-
-    println("Pekerjaaan kamu adalah $Pekerjaan dan alamat kamu di $Alamat")
+    //Variabel yang memiliki isi boleh tidak disebutkan tipe datanya
+    var umur = 22
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
